9d9c21acbb96b07ef3d6b32c842bbb22b06cd659
OngeziweMzizi <ongeziwemzizi42@gmail.com>
2023-08-09 11:29:32 -0700

    C - Static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,20 @@
 #include "main.h"
-
 /**
- * _puts - Prints a string .
- * @str: string.
+ *_puts - prints a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
  */
+
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
 
-	_putchar('\n');
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+return;
 }
