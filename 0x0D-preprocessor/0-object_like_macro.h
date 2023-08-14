776eb153f71a9bc491dc05506b0fe26a442be29e
OngeziweMzizi <ongeziwemzizi42@gmail.com>
2023-08-14 14:58:41 -0700

    Task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef O_OBJECT_LIKE_MACRO_H
+#define O_OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+/**
+ * File: 0-object_like_macro.h
+ * Description: Header file that defines a macro named
+ * SIZE as an abbreviation for the token 1024
+ */
+
+#endif /* OBJECT_LIKE_MACRO_H */
