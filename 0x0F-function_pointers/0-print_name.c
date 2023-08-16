93ffaa57b60d7a480d8145c7a2d644435a6e0c5f
OngeziweMzizi <ongeziwemzizi42@gmail.com>
2023-08-16 13:40:13 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name:name of the person
+ * @f: pointer points to function f
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+
+		f(name);
+}
