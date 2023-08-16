f7e5df0f27abf431e3dff35fa01fa000160eb235
OngeziweMzizi <ongeziwemzizi42@gmail.com>
2023-08-16 15:38:54 -0700

    Tasks updated

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers();
+    return (0);
+}
