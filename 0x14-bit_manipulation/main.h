27582cca69af5ed1df774c15f9f89044813af1c6
OngeziweMzizi <ongeziwemzizi42@gmail.com>
2023-09-01 02:52:25 -0700

    Updated

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char *create_array(unsigned int size, char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 int _putchar(char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
